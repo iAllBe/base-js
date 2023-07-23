649c1f2691253cf94c72606258ae97499dd29618
d.ridniy <allbegj@gmail.com>
2023-07-24 01:00:00 +0500

    lesson 6 done

---
+++ lesson_6/task_3.js
@@ -0,0 +1,17 @@
+﻿// Задание 3
+// Пользователь с клавиатуры вводит 3 числа, необходимо создать функцию, которая определяет максимальное значение среди этих чисел
+let number1 = prompt('Введите 1-ое число');
+let number2 = prompt('Введите 2-ое число');
+let number3 = prompt('Введите 3-ое число');
+console.log(`Максимальное значение: ${maxNumber(number1, number2, number3)}`);
+
+function maxNumber(number1, number2, number3) {
+
+    if (number1 > number2 && number1 > number3) {
+        return number1;
+    } else if (number2 > number1 && number2 > number3) {
+        return number2;
+    } else {
+        return number3;
+    }
+}
---
+++ lesson_6/task_4.js
@@ -0,0 +1,31 @@
+﻿// Необходимо реализовать четыре функции, каждая функция должна принимать по два числа и выполнять одну из операций (каждая функция выполняет одну из них):
+// 1. Сложение
+// 2. Разность
+// 3. Умножение
+// 4. Деление
+console.log(`Сумма: ${sum(5, 5)}`);
+console.log(`Разность: ${difference(10, 20)}`);
+console.log(`Умножение: ${multiplication(2, 5)}`);
+console.log(`Деление: ${segmentation(50, 5)}`);
+
+function sum(number1, number2) {
+    return number1 + number2;
+}
+
+function difference(number1, number2) {
+    if (number1 > number2) {
+        return number1 - number2;
+    } else if (number1 == number2) {
+        return 0;
+    } else {
+        return number2 - number1;
+    }
+}
+
+function multiplication(number1, number2) {
+    return number1 * number2;
+}
+
+function segmentation(number1, number2) {
+    return number1 / number2;
+}
